2d975dd2705d6d87686dbe805c6bec9af73d2b3f
NgNhatThanh <thanhtobot159@gmail.com>
2025-01-08 15:58:04 +0700

    init

---
+++ bdc-backend/src/test/java/com/app/bdc_backend/BdcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bdc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
